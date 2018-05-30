e9fa2546b09d2e52036f29b0bf4708d9328fe7f7
Jordan Bunke <jtb17@ic.ac.uk>
2018-05-30 21:33:43 +0100

    Partial implementation of data processing

--- src/emulator/emulator_data_processing.c
+++ src/emulator/emulator_data_processing.c
@@ -3,3 +3,48 @@
 //
 
 #include "emulator_data_processing.h"
+
+void execute_data_processing(emulator_state *state) {
+    data_processing *data_pro = state->data_pro;
+    u_int32_t result = 0;
+    u_int32_t operand_2;
+
+    if (data_pro->i) {
+        operand_2 = data_pro->op2 % (2 ^ 8);
+    } else {
+
+    }
+
+    switch (data_pro->opcode) {
+        case AND :
+            state->reg[data_pro->rd] = state->reg[data_pro->rn] & operand_2;
+            break;
+        case EOR :
+            state->reg[data_pro->rd] = state->reg[data_pro->rn] ^ operand_2;
+            break;
+        case SUB :
+            state->reg[data_pro->rd] = state->reg[data_pro->rn] - operand_2;
+            break;
+        case RSB :
+            state->reg[data_pro->rd] = operand_2 - state->reg[data_pro->rn];
+            break;
+        case ADD :
+            state->reg[data_pro->rd] = state->reg[data_pro->rn] + operand_2;
+            break;
+        case TST :
+            result = state->reg[data_pro->rn] & operand_2;
+            break;
+        case TEQ :
+            result = state->reg[data_pro->rn] ^ operand_2;
+            break;
+        case CMP :
+            result = state->reg[data_pro->rn] - operand_2;
+            break;
+        case ORR :
+            state->reg[data_pro->rd] = state->reg[data_pro->rn] | operand_2;
+            break;
+        case MOV :
+            state->reg[data_pro->rd] = operand_2;
+            break;
+    }
+}
--- src/emulator/emulator_data_processing.h
+++ src/emulator/emulator_data_processing.h
@@ -13,19 +13,20 @@ typedef struct cpsr {
 } cpsr;
 
 typedef enum {
-    AND = 0,
-    EOR = 1,
-    SUB = 2,
-    RSB = 3,
-    ADD = 4,
-    TST = 8,
-    TEQ = 9,
-    CMP = 10,
-    ORR = 12,
-    MOV = 13
+    AND = 0, /* rn AND op2 */
+    EOR = 1, /* rn EOR op2 */
+    SUB = 2, /* rn - op2 */
+    RSB = 3, /* op2 - rn */
+    ADD = 4, /* rn + op2 */
+    TST = 8, /* AND but not written */
+    TEQ = 9, /* EOR but not written */
+    CMP = 10, /* SUB but not written */
+    ORR = 12, /* rn OR op2 */
+    MOV = 13 /* op2 */
 } OPCODE;
 
 typedef struct data_processing {
+    OPCODE opcode;
     u_int32_t rn;
     u_int32_t rd;
     u_int32_t op2;
@@ -40,6 +41,6 @@ typedef struct emulator_state {
     cpsr *cpsr_flags;
 };
 
-void execute_data_proc(emulator_state *st);
+void execute_data_proc(emulator_state *state);
 
 #endif //ARM11_09_EMULATOR_DATA_PROCESSING_H
