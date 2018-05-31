df6cc8ac5303b76d7e0305d8ea23cf777a1b4e01
kapilan <km2717@imperial.ac.uk>
2018-06-01 00:49:27 +0100

    adapted multiply to use bit and register input output standards

--- src/emulator/emulator_multiply.h
+++ src/emulator/emulator_multiply.h
@@ -7,28 +7,5 @@
 
 #include <stdint.h>
 
-typedef struct multiply_instr {
-    int set;
-    int acc;
-    uint32_t rd; // destination
-    uint32_t rn; // operand
-    uint32_t rs; // operand
-    uint32_t rm; // operand
-    uint32_t operand2;
-} multiply_instr;
-
-typedef struct cpsrFlag {
-    int n;
-    int z;
-    int c;
-    int v;
-} cpsr;
-
-typedef struct state {
-    multiply_instr *multiply_instruction;
-    uint32_t *reg;
-    uint8_t *memory;
-    cpsr *cpsrFlag;
-} state;
-
+void multiply(uint32_t instr, struct CPUState cpu);
 #endif
--- src/emulator/utilities.c
+++ src/emulator/utilities.c
@@ -5,10 +5,7 @@
 #include "utilities.h"
 
 //  takes bits between begin and end --> [begin,end)
-uint32_t bits_extract(uint32_t value, int begin, int end)
-{
+uint32_t bits_extract(uint32_t value, int begin, int end) {
     uint32_t mask = (1 << (end - begin)) - 1;
     return (value >> begin) & mask;
 }
-
-
