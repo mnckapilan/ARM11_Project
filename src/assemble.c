f0fc41f011dff284a01fc93160caf6da4773df88
Sukant Roy <sukantroy@Sukants-MacBook-Pro.local>
2018-06-02 12:59:18 +0100

    Partial implementation ofmain function in assemble.c

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,23 @@
 #include <stdlib.h>
+#include "emulator_io.h"
+#include "emulator_constants.h"
+
+#define NO_EXPECTED_ARGS 3
 
 int main(int argc, char **argv) {
-    return EXIT_SUCCESS;
+    if (argc != NO_EXPECTED_ARGS) {
+        fprintf(stderr, "Assembler takes exactly two files as arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+    char* sourceFilePath = argv[1];
+    char* binaryFilePath = argv[2];
+
+    FILE* sourceFile = load_file(sourceFilePath, NUM_MEMORY_LOCATIONS);
+    FILE* binaryOutputFile = load_file(binaryFilePath, NUM_MEMORY_LOCATIONS);
+    if (sourceFile == NULL || binaryOutputFile == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+
+    exit(EXIT_SUCCESS);
 }
