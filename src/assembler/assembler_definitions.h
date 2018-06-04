063fc5f91bd6539ef219b667e3738036ac46c340
kapilan <km2717@imperial.ac.uk>
2018-06-04 17:23:21 +0100

    added instruction and OPCODE typedefs in assembler_definitions.h for use in assembler

---
+++ src/assembler/assembler_definitions.h
@@ -0,0 +1,44 @@
+//
+// Created by Kapilan M on 04/06/2018.
+//
+
+#ifndef ARM11_09_ASSEMBLER_DEFINITIONS_H
+#define ARM11_09_ASSEMBLER_DEFINITIONS_H
+
+#include <stdint.h>
+
+
+typedef struct instruction {
+
+    char *phrase;
+    uint32_t operand2;
+    uint32_t rd;
+    uint32_t rn;
+    uint32_t rs;
+    uint32_t rm;
+    uint32_t imm;
+    uint32_t expression;
+    uint32_t lastAdd;
+    uint32_t p;
+    uint32_t u;
+
+} instruction;
+
+
+//enums for OPCODEs
+typedef enum {
+    AND = 0,
+    EOR = 1,
+    SUB = 2,
+    RSB = 3,
+    ADD = 4,
+    TST = 8,
+    TEQ = 9,
+    CMP = 10,
+    ORR = 12,
+    MOV = 13
+} OPCODE;
+
+
+
+#endif //ARM11_09_ASSEMBLER_DEFINITIONS_H
