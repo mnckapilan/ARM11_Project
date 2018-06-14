e802d6289af95e8426e6300940cca345fec0b369
sukantroy <sr4617@imperial.ac.uk>
2018-06-14 01:22:56 +0100

    Added function declarations to header

--- src/emulator/block_data_transfer.h
+++ src/emulator/block_data_transfer.h
@@ -13,4 +13,12 @@
 
 uint32_t execute_block_data_transfer(uint32_t instruction, State cpu);
 
+uint32_t preIncrementAddressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList);
+
+uint32_t postIncrementAddressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList);
+
+uint32_t preDecrementAddressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList);
+
+uint32_t postDecrementAddressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList);
+
 #endif
