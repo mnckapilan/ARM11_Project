a3887c3ab78236f8d3b2c6372cf657ecf9410559
sukantroy <sr4617@imperial.ac.uk>
2018-06-14 16:07:37 +0100

    Added comments to emulator_block_data_transfer source file

--- src/emulator_block_data_transfer.c
+++ src/emulator_block_data_transfer.c
@@ -1,5 +1,12 @@
 #include "emulator_block_data_transfer.h"
 
+/* In pre-increment addressing, the address is incremented by the offset of 4 before the transfer of each register.
+ * If R15 (PC) is stored to memory, the stored value is the address of the STM instruction plus 12 (applicable to any of
+ * the addressing modes). Each bit in the register list corresponds to a register (1 means transfer, 0 means do not
+ * transfer). For increment addressing (pre or post), an up to for loop is used. The registers are transferred in the order
+ * lowest to highest for increment addressing mode. The lowest register gets transferred to or from the lowest memory address
+ * (for any addressing mode).
+ */
 uint32_t pre_increment_addressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList) {
     uint32_t addr = baseRegValue;
     uint32_t regTransferBit;
@@ -7,25 +14,41 @@ uint32_t pre_increment_addressing(State cpu, uint32_t lBit, uint32_t baseRegValu
         regTransferBit = bits_extract(regList, i, i + 1);
         if (regTransferBit == 1) {
             addr += BYTES_PER_WORD;
-            transfer_data(cpu, addr, lBit, i);
+            if ((lBit == 0) && (i == PC_INDEX)) {
+                uint32_t stmAddress = read_from_register(cpu, PC_INDEX);
+                write_to_memory(cpu, addr, stmAddress + BYTES_PER_WORD);
+            }
+            else {
+                transfer_data(cpu, addr, lBit, i);
+            }
         }
     }
     return addr;
 }
 
+// In post-increment addressing, the address is incremented by the offset after the transfer of each register.
 uint32_t post_increment_addressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList) {
     uint32_t addr = baseRegValue;
     uint32_t regTransferBit;
     for (uint32_t i = 0; i < REG_LIST_LENGTH; i++) {
         regTransferBit = bits_extract(regList, i, i + 1);
         if (regTransferBit == 1) {
-            transfer_data(cpu, addr, lBit, i);
+            if ((lBit == 0) && (i == PC_INDEX)) {
+                uint32_t stmAddress = read_from_register(cpu, PC_INDEX);
+                write_to_memory(cpu, addr, stmAddress + BYTES_PER_WORD);
+            }
+            else {
+                transfer_data(cpu, addr, lBit, i);
+            }
             addr += BYTES_PER_WORD;
         }
-     }
-      return addr;
+    }
+    return addr;
 }
 
+/* In pre-decrement addressing, the address is decremented by the offset before the transfer of each register. For
+ *  decrement addressing mode (pre/post), down to for loop is used. Registers are transferred in the order highest to lowest.
+ */
 uint32_t pre_decrement_addressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList) {
     uint32_t addr = baseRegValue;
     uint32_t regTransferBit;
@@ -33,25 +56,46 @@ uint32_t pre_decrement_addressing(State cpu, uint32_t lBit, uint32_t baseRegValu
         regTransferBit = bits_extract(regList, i, i + 1);
         if (regTransferBit == 1) {
             addr -= BYTES_PER_WORD;
-            transfer_data(cpu, addr, lBit, i);
+            if ((lBit == 0) && (i == PC_INDEX)) {
+                uint32_t stmAddress = read_from_register(cpu, PC_INDEX);
+                write_to_memory(cpu, addr, stmAddress + BYTES_PER_WORD);
+            }
+            else {
+                transfer_data(cpu, addr, lBit, i);
+            }
         }
     }
     return addr;
 }
 
+// In post-decrement addressing, the address is decremented by the offset after the transfer of each register.
 uint32_t post_decrement_addressing(State cpu, uint32_t lBit, uint32_t baseRegValue, uint32_t regList) {
     uint32_t addr = baseRegValue;
     uint32_t regTransferBit;
     for (uint32_t i = REG_LIST_LENGTH - 1; i >= 0; i--) {
         regTransferBit = bits_extract(regList, i, i + 1);
         if (regTransferBit == 1) {
-            transfer_data(cpu, addr, lBit, i);
+            if ((lBit == 0) && (i == PC_INDEX)) {
+                uint32_t stmAddress = read_from_register(cpu, PC_INDEX);
+                write_to_memory(cpu, addr, stmAddress + BYTES_PER_WORD);
+            }
+            else {
+                transfer_data(cpu, addr, lBit, i);
+            }
             addr -= BYTES_PER_WORD;
         }
     }
     return addr;
 }
 
+/* R15 (PC) should not been used as the base register in any LDM or STM instruction. The register list must be non-empty.
+ * Any subset of the 16 registers or all of them may be specified in the register list.
+ * The wBit is the write back bit, which determines whether write back of the modified base is required or not. An address
+ * is written to the base register if wBit equals 1. If wBit equals 0, the base register retains its original value,
+ * unless it was also in the transfer (register) list of a LDM instruction - overwritten by new loaded value. S bit is
+ * ignored as PSR register is ignored. The registers are transferred in the order lowest to highest. The lowest register
+ * gets transferred to or from the lowest memory address (for any addressing mode).
+ */
 uint32_t block_data_transfer(uint32_t instruction, State cpu) {
     if (check_condition(instruction, cpu) == 0) {
         return 0;
