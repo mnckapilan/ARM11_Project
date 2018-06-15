54385e94f20eae7dfebfc4999bccbb8d49249877
Jordan Bunke <jtb17@ic.ac.uk>
2018-06-15 02:50:36 +0100

    Reverted and merged to reclaim lost files

---
+++ src/assembler_singleDataTransfer.h
@@ -0,0 +1,15 @@
+//
+// Created by Jordan Bunke on 2018-06-08.
+//
+
+#ifndef ARM11_09_ASSEMBLER_SINGLEDATATRANSFER_H
+#define ARM11_09_ASSEMBLER_SINGLEDATATRANSFER_H
+
+#include "assembler_utilities.h"
+#include <math.h>
+
+#define COND 14u
+
+uint32_t single_data_transfer(instruction *ins);
+
+#endif //ARM11_09_ASSEMBLER_SINGLEDATATRANSFER_H
