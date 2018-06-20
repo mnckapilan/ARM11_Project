bd07372bb890c5db2e08fa30c49ba88096ea07eb
Jordan Bunke <jtb17@ic.ac.uk>
2018-06-20 14:49:58 +0100

    Added block data transfer to repo

---
+++ src/assembler_block_data_transfer.c
@@ -0,0 +1,23 @@
+//
+// Created by Sukant Roy on 13/06/2018.
+//
+
+#include "assembler_block_data_transfer.h"
+
+uint32_t block_data_transfer(instruction *ins) {
+
+    uint32_t condition = COND << 28u;
+    uint32_t p = (ins->p) << 24u;
+    uint32_t u = (ins->u) << 23u;
+    uint32_t l = 1;
+    uint32_t rn = (ins->rn) << 16u;
+    uint32_t s = (ins->s) << 22u;
+    uint32_t w = (ins->w) << 21u;
+    uint32_t regList = ins->regList;
+
+    if((ins->phrase)[0] == 's') {
+        l = 0;
+    }
+
+    return condition | (1u << 27u) | p | u | s | w | (l << 20u ) | rn | regList;
+}
---
+++ src/assembler_block_data_transfer.h
@@ -0,0 +1,14 @@
+//
+// Created by Sukant Roy on 13/06/2018.
+//
+
+#ifndef ARM11_09_ASSEMBLER_BLOCKDATATRANSFER_H
+#define ARM11_09_ASSEMBLER_BLOCKDATATRANSFER_H
+
+#include "assembler_utilities.h"
+
+#define COND 14u
+
+uint32_t block_data_transfer(instruction *ins);
+
+#endif
