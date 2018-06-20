32a75e241af06888af7d9d1a6537f2ac2791af05
Jordan Bunke <jtb17@ic.ac.uk>
2018-06-20 14:12:09 +0100

    Implemented branch with link. The R14 handling should be done in set_instruction.

--- src/assembler_branch.c
+++ src/assembler_branch.c
@@ -8,22 +8,17 @@ uint32_t branch(instruction *ins, uint32_t current_address) {
     int offset = ins->expression - current_address - PIPELINE;
     /* type is the 2 char string for the cond type */
     char type[2];
-    type[0] = (ins->phrase)[1];
-    type[1] = (ins->phrase)[2];
+
+    uint32_t link_flag = 0;
+
+    if ((ins->phrase)[1] == 'l') {
+        link_flag = 1;
+    }
+
+    type[0] = (ins->phrase)[2 - link_flag];
+    type[1] = (ins->phrase)[3 - link_flag];
     uint32_t condition = getCond(type);
 
-    return (condition << 28u) | (5u << 25u) | ((offset >> 2u) & 0x00ffffff);
-}
-
-uint32_t branch_with_link(instruction *ins, uint32_t current_address) {
-    int offset = ins->expression - current_address - PIPELINE;
-
-    char type[2];
-    type[0] = (ins->phrase)[1];
-    type[1] = (ins->phrase)[2];
-    uint32_t condition = getCond(type);
-
-    // link bit flag encoded
-    return (condition << 28u) | (5u << 25u) | (1u << 24u) |
+    return (condition << 28u) | (5u << 25u) | (link_flag << 24u) |
            ((offset >> 2u) & 0x00ffffff);
 }
